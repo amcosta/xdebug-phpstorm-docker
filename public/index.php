98146f4505ccf40384e8ba61e462a14111689437
Alex Moreno <alex.moreno.costa@gmail.com>
2020-01-19 22:38:20 -0300

    Alterações nas configurações para rodar o xdebug sem adicionar variaveis de ambiente no container

--- public/index.php
+++ public/index.php
@@ -1,5 +1,21 @@
 <?php
 
-$total = 2 * 6;
+try {
+    $pdo = new PDO(
+        'mysql:host=mysql;dbname=test', 'root', 'root',
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+} catch (Exception $exception) {
+    echo $exception->getTraceAsString();
+}
+
+$names = ["Peĺicula 3D", "Película de vidro", "Capa anti-impacto", "Caderno", "Chips", "Cartão de memória", "Caneta"];
+foreach ($names as $name) {
+    $stmt = $pdo->prepare("INSERT INTO products (name) VALUES (':name')");
+    $stmt->bindValue(':name', $name);
+    $stmt->execute();
+}
+
+$total = 3 * 6;
 
 echo "Total {$total}";
